cb432929e9f414473f91c304ccdadeaf88d3468c
APMuley <122552149+APMuley@users.noreply.github.com>
2025-09-18 19:03:02 +0530

    Update sysproc.c

--- sysproc.c
+++ sysproc.c
@@ -1,73 +1,80 @@
 #include "types.h"
-#include "x86.h"
+#include "riscv.h"
 #include "defs.h"
-#include "date.h"
 #include "param.h"
 #include "memlayout.h"
-#include "mmu.h"
+#include "spinlock.h"
 #include "proc.h"
+#include "vm.h"
 
-int
-sys_fork(void)
-{
-  return fork();
-}
-
-int
+uint64
 sys_exit(void)
 {
-  exit();
+  int n;
+  argint(0, &n);
+  kexit(n);
   return 0;  // not reached
 }
 
-int
-sys_wait(void)
-{
-  return wait();
-}
-
-int
-sys_kill(void)
-{
-  int pid;
-
-  if(argint(0, &pid) < 0)
-    return -1;
-  return kill(pid);
-}
-
-int
+uint64
 sys_getpid(void)
 {
   return myproc()->pid;
 }
 
-int
+uint64
+sys_fork(void)
+{
+  return kfork();
+}
+
+uint64
+sys_wait(void)
+{
+  uint64 p;
+  argaddr(0, &p);
+  return kwait(p);
+}
+
+uint64
 sys_sbrk(void)
 {
-  int addr;
+  uint64 addr;
+  int t;
   int n;
 
-  if(argint(0, &n) < 0)
-    return -1;
+  argint(0, &n);
+  argint(1, &t);
   addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+
+  if(t == SBRK_EAGER || n < 0) {
+    if(growproc(n) < 0) {
+      return -1;
+    }
+  } else {
+    // Lazily allocate memory for this process: increase its memory
+    // size but don't allocate memory. If the processes uses the
+    // memory, vmfault() will allocate it.
+    if(addr + n < addr)
+      return -1;
+    myproc()->sz += n;
+  }
   return addr;
 }
 
-int
-sys_sleep(void)
+uint64
+sys_pause(void)
 {
   int n;
   uint ticks0;
 
-  if(argint(0, &n) < 0)
-    return -1;
+  argint(0, &n);
+  if(n < 0)
+    n = 0;
   acquire(&tickslock);
   ticks0 = ticks;
   while(ticks - ticks0 < n){
-    if(myproc()->killed){
+    if(killed(myproc())){
       release(&tickslock);
       return -1;
     }
@@ -77,9 +84,18 @@ sys_sleep(void)
   return 0;
 }
 
+uint64
+sys_kill(void)
+{
+  int pid;
+
+  argint(0, &pid);
+  return kkill(pid);
+}
+
 // return how many clock tick interrupts have occurred
 // since start.
-int
+uint64
 sys_uptime(void)
 {
   uint xticks;
@@ -89,3 +105,38 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64
+sys_getppid(void)
+{
+  return myproc()->parent->pid;
+}
+
+uint64
+sys_getprio(void)
+{
+  struct proc *p = myproc();
+  acquire(&p->lock);
+  int priority = p->priority;
+  release(&p->lock);
+  return priority;
+}
+
+uint64
+sys_setprio(void) 
+{
+  int priority;
+
+  argint(0, &priority);
+
+  struct proc *p = myproc();
+  acquire(&p->lock);
+  p->priority = priority;
+  release(&p->lock);
+  return 0;
+}
+
+// declare the function
+uint64 sys_cprintf_locked(void);
+
+
